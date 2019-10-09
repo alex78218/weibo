49f7788130c4b095bb9409eff37cd95f7c6033ee
Alex78218 <sa130494625@qq.com>
2019-10-09 04:37:38 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/help', 'StaticPagesController@hellp')->name('help');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
