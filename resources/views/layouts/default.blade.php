cccf239554f3bcc2f553bf6014cf4e9341eaf140
Alex78218 <sa130494625@qq.com>
2019-10-09 07:54:16 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,9 +9,14 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+
         @yield('content')
-        @include('layouts._footer')
+
+
       </div>
+
     </div>
+    @include('layouts._footer')
   </body>
 </html>
